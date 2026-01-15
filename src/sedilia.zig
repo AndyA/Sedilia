696db43200276bd3f1f3fb1fe49f77745403efb8
Andy Armstrong <andy@hexten.net>
2026-01-15 12:31:12 +0000

    A test!

--- src/sedilia.zig
+++ src/sedilia.zig
@@ -1,8 +1,27 @@
 const std = @import("std");
 const rocksdb = @import("rocksdb");
 
-pub fn main() !void {
-    const gpa = std.heap.page_allocator;
+pub fn main(init: std.process.Init) !void {
+    try std.Io.Dir.createDirPath(std.Io.Dir.cwd(), init.io, "tmp");
+
+    var err: ?rocksdb.Data = null;
+    const db, const cf = try rocksdb.DB.open(
+        init.gpa,
+        "tmp/db.couchdb",
+        .{ .create_if_missing = true },
+        null,
+        &err,
+    );
+    defer db.deinit();
+    defer init.gpa.free(cf);
+    std.debug.print("{any}\n", .{cf});
+}
+
+test "scratch" {
+    const gpa = std.testing.allocator;
+    const io = std.testing.io;
+    try std.Io.Dir.createDirPath(std.Io.Dir.cwd(), io, "tmp");
+
     var err: ?rocksdb.Data = null;
     const db, const cf = try rocksdb.DB.open(
         gpa,
@@ -12,8 +31,9 @@ pub fn main() !void {
         &err,
     );
     defer db.deinit();
-    // _ = cf;
-    std.debug.print("{any}\n", .{cf});
+    defer gpa.free(cf);
+
+    // std.debug.print("{any}\n", .{cf});
 }
 
 test {
