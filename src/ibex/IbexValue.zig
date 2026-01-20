dc707decfbb4c368a3ebf7bef0de52bbd51fd199
Andy Armstrong <andy@hexten.net>
2026-01-20 20:08:14 +0000

    Minor

--- src/ibex/IbexValue.zig
+++ src/ibex/IbexValue.zig
@@ -1,6 +1,22 @@
 const std = @import("std");
 
-// An IbexValue can include literal fields of JSON or Ibex/Oryx
+pub const IbexClass = struct {}; // TODO
+
+pub const IbexValue = union(u8) {
+    const Self = @This();
+
+    null,
+    boolean: bool,
+    int: i64,
+    float: f64,
+    array: []Self,
+    object: []Self, // like an array but the first item must be a class
+    string: []u8,
+    class: *IbexClass,
+    // an IbexValue can include literal fields of JSON or Ibex/Oryx
+    json: []u8,
+    ibex: []u8,
+};
 
 test {
     // std.debug.print("Hello!\n", .{});
