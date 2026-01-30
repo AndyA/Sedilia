13055c505fd5ec61ed15cdaac06ebc3f64db2f0a
Andy Armstrong <andy@hexten.net>
2026-01-30 01:32:55 +0000

    Minor

--- src/ibex/Literal.zig
+++ src/ibex/Literal.zig
@@ -1,12 +1,8 @@
-const std = @import("std");
 const IbexError = @import("./ibex.zig").IbexError;
-
 const IbexWriter = @import("./IbexWriter.zig");
-
-const Self = @This();
 
 ibex: []const u8,
 
-pub fn writeIbex(self: *const Self, w: *IbexWriter) IbexError!void {
+pub fn writeIbex(self: *const @This(), w: *IbexWriter) IbexError!void {
     try w.writeBytes(self.ibex);
 }
